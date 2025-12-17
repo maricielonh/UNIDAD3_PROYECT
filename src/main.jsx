d7b0c4540604e1c006be24c27eb7a37577781f1d
maricielo-narro <marnarrohuarac@crackthecode.la>
2025-12-16 19:58:38 -0500

    fix router

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,21 @@
-
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import "./firebase"; // asegúrate de que el path sea correcto
+import { BrowserRouter } from "react-router-dom";
 
-// 🧩 Importa el AuthProvider
+import App from "./App.jsx";
+import "./firebase";
+
 import { AuthProvider } from "./context/AuthContext";
 
-// 🧩 Importa Bootstrap CSS y JS
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/*  Aquí envolvemos toda la app con el AuthProvider */}
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <BrowserRouter basename="/UNIDAD3_PROYECT">
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
   </StrictMode>
 );
-
-
