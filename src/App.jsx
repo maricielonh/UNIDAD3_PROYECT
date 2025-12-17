f2126fabd15f519b1a0e9ad593a72dc3ce968bfd
maricielo-narro <marnarrohuarac@crackthecode.la>
2025-12-16 20:03:07 -0500

    fix duplicate

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,37 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./Home";
 import Nosotras from "./Nosotras";
 import Valores from "./Valores";
 import Contacto from "./Contacto";
 import Login from "./pages/Login";
-import { AuthProvider } from "./auth/AuthProvider";
 import Dashboard from "./pages/Dashboard";
 import ProtectedRoute from "./auth/ProtectedRoute";
 import Publicidad from "./pages/Publicidad";
 
-
 function App() {
   return (
-    <BrowserRouter>
-      <AuthProvider>
-        <Routes>
+    <Routes>
+      {/* Redirige raíz al Home */}
+      <Route path="/" element={<Navigate to="/Home" />} />
 
-          {/* Redirige raíz al login */}
-          <Route path="/" element={<Navigate to="/Home" />} />
+      {/* Rutas públicas */}
+      <Route path="/login" element={<Login />} />
+      <Route path="/Home" element={<Home />} />
+      <Route path="/Nosotras" element={<Nosotras />} />
+      <Route path="/Valores" element={<Valores />} />
+      <Route path="/Contacto" element={<Contacto />} />
+      <Route path="/publicidad" element={<Publicidad />} />
 
-          {/* Rutas públicas */}
-          <Route path="/login" element={<Login />} />
-          <Route path="/Home" element={<Home />} />
-          <Route path="/Nosotras" element={<Nosotras />} />
-          <Route path="/Valores" element={<Valores />} />
-          <Route path="/Contacto" element={<Contacto />} />
-          <Route path="/publicidad" element={<Publicidad />} />
-          <Route
-            path="/dashboard"
-            element={
-              <ProtectedRoute>
-                <Dashboard />
-              </ProtectedRoute>
-            }
-          />
-          
-
-        </Routes>
-      </AuthProvider>
-    </BrowserRouter>
+      {/* Ruta protegida */}
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
+    </Routes>
   );
 }
 
